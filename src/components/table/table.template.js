b57cc38404cc447e8a8d95142c7e361c4f1c906f
Valentin <qweqweq@qwe>
2020-08-05 14:25:03 +0300

    Fix all

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,54 +3,42 @@ const CODES = {
     Z: 90
 }
 
-
-function toCell(_, index) {
-  return `
-    <div class="cell" contenteditable data-col="${index}">
-    </div>
+function toCell(_, col) {
+    return `
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
 function toColumn(col, index) {
     return `
-    <div class="column" data-type="resizable" data-col="${index}">${col}
-     <div class="col-resize" data-resize="col"></div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
     </div>
-function createCell(cell) {
-  return `
-    <div class="cell" contenteditable>${cell}</div>
   `
 }
 
-function toColumn(col) {
-    return `
-    <div class="column">${col}</div>
-  `
-}
-
-function createRow(content, num='') {
-    const resizer= num !==''?` 
-    <div class="row-resize" data-resize="row"></div>`:'';
+function createRow(index, content) {
+    // eslint-disable-next-line max-len
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
     return `
     <div class="row" data-type="resizable">
-      <div class="row-info">${num}
-      ${resizer}
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
       </div>
-    return `
-    <div class="row">
-      <div class="row-info">${num}</div>
       <div class="row-data">${content}</div>
     </div>
   `
 }
 
 function toChar(_, index) {
-    return String.fromCharCode(CODES.A + index);
+    return String.fromCharCode(CODES.A + index)
 }
 
 export function createTable(rowsCount = 15) {
-    const colsCount = CODES.Z - CODES.A + 1;
-    const rows = [];
+    const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
+    const rows = []
 
     const cols = new Array(colsCount)
         .fill('')
@@ -58,17 +46,16 @@ export function createTable(rowsCount = 15) {
         .map(toColumn)
         .join('')
 
-    rows.push(createRow(cols));
+    rows.push(createRow(null, cols))
 
     for (let i = 0; i < rowsCount; i++) {
-        const line = new Array(colsCount)
+        const cells = new Array(colsCount)
             .fill('')
-            .map(toChar)
             .map(toCell)
-            .map(createCell)
             .join('')
-        rows.push(createRow(line, i+1))
+
+        rows.push(createRow(i + 1, cells))
     }
 
-    return rows.join('');
+    return rows.join('')
 }
