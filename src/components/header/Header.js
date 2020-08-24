3f1971b6ba34c83f3933c09d70be2c14041fe4b3
Valentin <qweqweq@qwe>
2020-08-24 15:59:38 +0300

    Finish redux

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
+import {changeTitle} from '@/redux/actions';
+import {defaultTitle} from '@/constans';
+import {debounce} from '@core/utils';
 
 export class Header extends ExcelComponent {
     static className = 'excel__header';
     constructor($root, options) {
         super($root, {
             name: 'Header',
+            listeners: ['input'],
             ...options,
         });
     }
+    prepare() {
+        this.onInput= debounce(this.onInput)
+    }
+
     toHTML() {
-        return `  <input type="text" class="input" value="Новая таблица">
+        const title = this.store.getState().title || defaultTitle;
+        return `  <input type="text" class="input" value="${title}">
             <div>
                 <div class="button">
                     <i class="material-icons">exit_to_app</i>
@@ -18,5 +28,9 @@ export class Header extends ExcelComponent {
                     <i class="material-icons">delete</i>
                 </div>
             </div>`
+    }
+    onInput(event) {
+        const $target = $(event.target);
+        this.$dispatch(changeTitle($target.text()))
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import './scss/index.scss';
-import {Excel} from '@/components/excel/Excel';
-import {Header} from '@/components/header/Header';
-import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Formula} from '@/components/formula/Formula';
-import {Table} from '@/components/table/Table';
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
+import {createStore} from '@core/createStore'
+import {storage, debounce} from '@core/utils'
+import {rootReducer} from '@/redux/rootReducer'
+import './scss/index.scss'
+import {initialState} from '@/redux/initialState';
+
+const store = createStore(rootReducer, initialState)
+
+const stateListener = debounce(state => {
+    console.log('App State: ', state)
+    storage('excel-state', state)
+}, 300)
+
+store.subscribe(stateListener)
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
-});
-excel.render();
+    components: [Header, Toolbar, Formula, Table],
+    store
+})
+
+excel.render()
